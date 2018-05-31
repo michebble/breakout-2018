bdd4c368dee8fb77b448b480e120c0c235100aa2
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-31 15:34:06 +1000

    improve ball positioning

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,18 +21,29 @@ function preload() {
   game.load.image('ball', './assets/img/ball.png');
 }
 function create() {
+  //ball
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  ball = game.add.sprite(50, 50, 'ball');
+  ball = game.add.sprite(
+    game.world.width * 0.5, 
+    game.world.height - 25, 
+    'ball');
+  ball.anchor.set(0.5);
   game.physics.enable(ball, Phaser.Physics.ARCADE);
-  ball.body.velocity.set(150, 150);
+  ball.body.velocity.set(150, -150);
   ball.body.collideWorldBounds = true;
   ball.body.bounce.set(1);
+
+    // paddle
   paddle = game.add.sprite(
     game.world.width * 0.5, 
     game.world.height - 5, 
     'paddle');
   paddle.anchor.set(0.5,1);
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  paddle.body.immovable = true;
+  paddle.body.collideWorldBounds = true;
 }
 function update() {
- 
+  game.physics.arcade.collide(ball, paddle);
+  paddle.x = game.input.x || game.world.width * 0.5;
 }
