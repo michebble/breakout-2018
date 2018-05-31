5a1cc17f8c873795bd0b7ef85d0a0033ac3bd62f
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-31 14:59:34 +1000

    set up app.js

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+console.log('live')
+
+var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
+  preload: preload, create: create, update: update
+});
+function preload() {}
+function create() {}
+function update() {}
