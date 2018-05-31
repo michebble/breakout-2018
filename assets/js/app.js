0d90181023b75c42791de1abfec6f0238d332897
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-31 16:09:59 +1000

    add scoring and lives

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,11 @@ var paddle;
 var bricks;
 var newBrick;
 var brickInfo;
+var scoreText;
+var score = 0;
+var lives = 3;
+var livesText;
+var lifeLostText;
 
 function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -37,9 +42,7 @@ function create() {
   ball.body.bounce.set(1);
   ball.checkWorldBounds = true;
   game.physics.arcade.checkCollision.down = false;
-  ball.events.onOutOfBounds.add(function(){
-    location.reload();
-  }, this);
+  ball.events.onOutOfBounds.add(ballLeaveScreen, this);
 
     // paddle
   paddle = game.add.sprite(
@@ -53,9 +56,18 @@ function create() {
 
   // bricks
   initBricks()
+
+  // scoring
+  scoreText = game.add.text(5, 5, 'Points: 0', { font: '18px Arial', fill: '#0095DD' });
+  livesText = game.add.text(game.world.width-5, 5, 'Lives: '+lives, { font: '18px Arial', fill: '#0095DD' });
+  livesText.anchor.set(1,0);
+  lifeLostText = game.add.text(game.world.width*0.5, game.world.height*0.5, 'Life lost, click to continue', { font: '18px Arial', fill: '#0095DD' });
+  lifeLostText.anchor.set(0.5);
+  lifeLostText.visible = false;
 }
 function update() {
   game.physics.arcade.collide(ball, paddle);
+  game.physics.arcade.collide(ball, bricks, ballHitBrick);
   paddle.x = game.input.x || game.world.width * 0.5;
 }
 
@@ -73,7 +85,7 @@ function initBricks() {
     },
     padding: 10
   }
-  brick = game.add.group();
+  bricks = game.add.group();
   for(c = 0; c < brickInfo.count.col; c++) {
     for(r = 0; r < brickInfo.count.row; r++ ) {
       var brickX = (c * (brickInfo.width + brickInfo.padding)) + brickInfo.offset.left;
@@ -83,7 +95,41 @@ function initBricks() {
       game.physics.enable(newBrick, Phaser.Physics.ARCADE);
       newBrick.body.immovable = true;
       newBrick.anchor.set(0.5);
-      brick.add(newBrick);
+      bricks.add(newBrick);
     }
+  }
+}
+function ballHitBrick(ball, brick) {
+  brick.kill();
+  score += 10;
+  scoreText.setText('Points: '+score);
+
+  var count_alive = 0;
+  for (i = 0; i < bricks.children.length; i++) {
+    if (bricks.children[i].alive == true) {
+      count_alive++;
+    }
+  }
+  if (count_alive == 0) {
+    
+    location.reload();
+  }
+}
+
+function ballLeaveScreen() {
+  lives--;
+  if(lives) {
+      livesText.setText('Lives: '+lives);
+      lifeLostText.visible = true;
+      ball.reset(game.world.width * 0.5, game.world.height - 25);
+      paddle.reset(game.world.width * 0.5, game.world.height - 5);
+      game.input.onDown.addOnce(function(){
+          lifeLostText.visible = false;
+          ball.body.velocity.set(150, -150);
+      }, this);
+  }
+  else {
+      alert('You lost, game over!');
+      location.reload();
   }
 }
