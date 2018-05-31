f147cf193d6bab237ca3b1dd9461589678f0acdf
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-31 15:13:20 +1000

    add ball sprite

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,22 @@
 console.log('live')
 
 var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
-  preload: preload, create: create, update: update
+  preload: preload, 
+  create: create, 
+  update: update
 });
+
+var ball;
+
 function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   game.scale.pageAlignHorizontally = true;
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = '#eee';
+
+  game.load.image('ball', './assets/img/ball.png');
 }
-function create() {}
+function create() {
+  ball = game.add.sprite(50, 50, 'ball');
+}
 function update() {}
