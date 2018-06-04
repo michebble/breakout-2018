2af278f564d7b7653e4f681bcdf847f853313c7d
Michael Hebblethwaite <mhhebble@gmail.com>
2018-06-04 11:59:46 +1000

    prepare for branch

--- assets/js/app.js
+++ assets/js/app.js
@@ -210,3 +210,5 @@ function startGame() {
   ball.body.velocity.set(randomX, -200);
   playing = true;
 }
+
+var letters = ['_','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
